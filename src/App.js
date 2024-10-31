14e246da467cb6af8596a0c6db2b2f4a8cd70e0e
AmirhosseinKaviani <amirhk120@gmail.com>
2024-10-31 16:49:14 -0700

    Build Crypto Currancy App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [currancy, setCurrancy] = useState({})
+  const searchHandler =(e)=> {
+    setSearch(e.target.value);
+  }
+  useEffect(()=>{
+    axios.get('https://openapiv1.coinstats.app/coins',{
+      headers: {'X-API-KEY': '6ZUNZzAn330T+TQ3+VWRVrAO6lchaZ3L4ge7b4c2V54='}
+    }
+    ).then((response)=>setCurrancy(response.data.result))
+  },[])
+  console.log(currancy)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Crypto Currency App By Amirhossein</h2>
+      <input type="text" name='searchBar' onChange={searchHandler} placeholder='Search...'/>
+      <table>
+        <thread>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Market Cap</th>
+            <th>Price</th>
+            <th>Available Supply</th>
+            <th>Volume(24hr)</th>
+          </tr>
+        </thread>
+        <tbody>
+          
+        </tbody>
+      </table>
     </div>
   );
 }
