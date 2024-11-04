16a43f6cc2e5700bac03c2b4021c2344fc4b2264
AmirhosseinKaviani <amirhk120@gmail.com>
2024-11-03 16:29:14 -0800

    complete the projet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 function App() {
   const [search, setSearch] = useState("");
-  const [currancy, setCurrancy] = useState({})
+  const [currancy, setCurrancy] = useState([])
   const searchHandler =(e)=> {
     setSearch(e.target.value);
   }
@@ -13,15 +13,15 @@ function App() {
     axios.get('https://openapiv1.coinstats.app/coins',{
       headers: {'X-API-KEY': '6ZUNZzAn330T+TQ3+VWRVrAO6lchaZ3L4ge7b4c2V54='}
     }
-    ).then((response)=>setCurrancy(response.data.result))
+    ).then((response)=>{ setCurrancy(response.data.result) })
   },[])
-  console.log(currancy)
+
   return (
     <div className="App">
       <h2>Crypto Currency App By Amirhossein</h2>
       <input type="text" name='searchBar' onChange={searchHandler} placeholder='Search...'/>
       <table>
-        <thread>
+        <thead>
           <tr>
             <th>Rank</th>
             <th>Name</th>
@@ -29,11 +29,29 @@ function App() {
             <th>Market Cap</th>
             <th>Price</th>
             <th>Available Supply</th>
-            <th>Volume(24hr)</th>
+            <th>Volume(24)</th>
           </tr>
-        </thread>
+        </thead>
         <tbody>
-          
+        {
+          currancy.filter((val) => ( val.name.toLowerCase().includes(search.toLowerCase()) )
+    ).map((val) => {
+      return <tr>
+        <td className='rank'>{val.rank}</td>
+        <td className='logo'>
+          <a href={val.websiteUrl}>
+            <img src={val.icon}/>
+          </a>
+          <p>{val.name}</p>
+        </td>
+        <td className='symbol'>${val.marketCap}</td>
+        <td>${val.price.toFixed(2)}</td>
+        <td>{val.availablesupply}</td>
+        <td>{val.volume.toFixed(0)}</td>
+      </tr>
+    }
+    )
+        }
         </tbody>
       </table>
     </div>
